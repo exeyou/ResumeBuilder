f20efb646dea6cc951ee4ec5afeca09dbf16f3fa
exeyou <arsenstukalov@gmail.com>
2025-06-23 20:14:41 +0300

    Secured secret key

--- config/settings.py
+++ config/settings.py
@@ -11,20 +11,21 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import  load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR/".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8m_k1az*a1^7j$o27es+t^t+%7nt5@xzs4-x@61%50g*8_ivce'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG")=="True"
 
 ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1']
 
