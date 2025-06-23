2371e950616c84e4a0c6fcf7fa318f50b5037d59
exeyou <arsenstukalov@gmail.com>
2025-06-23 20:40:27 +0300

    Added allowed host

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")=="True"
 
-ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'quickprint.pythonanywhere.com']
 
 
 # Application definition
